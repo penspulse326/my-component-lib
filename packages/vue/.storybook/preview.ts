93f7745e60f02680be97442e543ba9d7833ef161
Shin <penspulse@gmail.com>
2026-02-03 17:25:05 +0800

    refactor: configure theme structure

--- packages/vue/.storybook/preview.ts
+++ packages/vue/.storybook/preview.ts
@@ -1,4 +1,6 @@
-import type { Preview } from '@storybook/vue3-vite'
+import type { Preview } from '@storybook/vue3-vite';
+
+import '@my-component-lib/theme/index.scss';
 
 const preview: Preview = {
   parameters: {
@@ -6,15 +8,15 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
+      test: 'todo',
     },
 
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
-    }
+    },
   },
 };
 
