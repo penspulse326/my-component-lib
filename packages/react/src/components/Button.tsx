7d0bfff2c86822dafcdf4d6455f76ca3b6115bb8
Shin <penspulse@gmail.com>
2026-02-04 17:48:22 +0800

    feat(react): add date attributes to replace props states

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -1,57 +1,35 @@
 import '@my-component-lib/theme/components/_button.scss';
-import clsx from 'clsx';
+import type { ButtonHTMLAttributes } from 'react';
 
-/**
- * Button
- * 按鈕元件，固定 type="button"
- */
-interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   /**
-   * 可以自行插入文字或元件
+   * 可以插入文字或元件
    */
   children: React.ReactNode;
 
   /**
-   * 自訂 className，套用於按鈕最外層
-   */
-  className?: string;
-
-  /**
    * 是否為載入中
    */
-  isLoading?: boolean;
-
-  /**
-   * 點擊事件
-   */
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-
-  /**
-   * 自訂內聯樣式，套用於按鈕最外層
-   */
-  style?: React.CSSProperties;
+  loading?: boolean;
 }
 
 function Button({
   children,
   className,
-  isLoading,
-  onClick,
-  style,
+  disabled,
+  loading,
+  ...props
 }: ButtonProps) {
-  const buttonClassName = clsx(
-    'my-button',
-    {
-      'is-loading': isLoading,
-    },
-    className
-  );
-
   return (
     <button
-      className={buttonClassName}
-      onClick={onClick}
-      style={style}
+      {...props}
+      aria-busy={loading}
+      aria-disabled={disabled || loading}
+      className={`my-button ${className}`}
+      data-disabled={disabled ? '' : undefined}
+      data-loading={loading ? '' : undefined}
+      data-state={loading ? 'loading' : disabled ? 'disabled' : 'idle'}
+      disabled={disabled || loading}
       type="button"
     >
       {children}
